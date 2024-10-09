ad870ddc13eac790a427656f3ffdc2d475ec407a
antsuarz <uo270543@uniovi.es>
2024-10-09 17:42:46 +0200

    Datos de los cargadores electricos de asturias

---
+++ datos/Cargadores_202410091740.sql
@@ -0,0 +1,101 @@
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Paseo de La Florida (Ayunt. Oviedo)','43.367574','-5.876762','CSS, Tipo 2','Socket Only, Tethered Connector','22kw,100kw'),
+	 ('Mercadona La Florida','43.366521','-5.872562','Tipo 2','Socket Only','22kw'),
+	 ('Calle José María Martínez Cachero (Ayunt. Oviedo)','43.363554','-5.874963','Tipo 2','Tethered Connection','22kw'),
+	 ('Calle Alejandro Casona (Ayunt. Oviedo)','43.362473','-5.865971','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle de la Regenta (Ayunt. Oviedo)','43.360273','-5.86536','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Julián Clavería (Ayunt. Oviedo)','43.354286','-5.868774','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Celestino Álvarez (Ayunt. Oviedo)','43.352049','-5.866254','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Piloña (Ayunt. Oviedo)','43.352152','-5.861924','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle El Valle (Ayunt. Oviedo)','43.354074','-5.862269','Tipo 2','Tethered Connector','22kw'),
+	 ('Mercadona Montecerráu','43.35465','-5.860018','Tipo 2','Socket Only','3.7kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Palacio de Congresos Oviedo (Ayunt. Oviedo)','43.357476','-5.861647','CCS','Socket Only','120kw'),
+	 ('Calle Hangzhou (Ayunt. Oviedo)','43.374259','-5.857427','Tipo 2','Socket Only','22kw'),
+	 ('Calle Peña Santa de Enol (Ayunt. Oviedo)','43.366209','-5.857297','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Arquitecto Reguera (Ayunt. Oviedo)','43.364687','-5.855464','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Marqués de Pidal (Ayunt. Oviedo)','43.364136','-5.853309','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Luis Fernández-Vega Sanz (Ayunt. Oviedo)','43.360218','-5.854765','Tipo 2','Tethered Connector','22kw'),
+	 ('Plaza de España (Ayunt. Oviedo)','43.359833','-5.852479','Tipo 2','Tethered Connector','22kw'),
+	 ('Univ. Oviedo Campus Llamaquique (Zunder)','43.35827','-5.854209','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle Leopoldo Calvo Sotelo (Ayunt. Oviedo)','43.357654','-5.853122','Tipo 2','Socket Only','22kw'),
+	 ('Parking Plaza de la Gesta de Oviedo','43.357892','-5.852141','Tipo 2','Socket Only','7.4kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Calle Valdés Salas (Ayunt. Oviedo)','43.355211','-5.851106','Tipo 2','Tethered Connector','22kw'),
+	 ('Univ. Oviedo Campus Catalanes (Zunder)','43.354768','-5.851746','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle de Otero (Ayunt. Oviedo)','43.353887','-5.838145','Tipo 2','Socket Only','22kw'),
+	 ('Calle Arzobispo Guisasola (Ayunt. Oviedo)','43.358491','-5.844243','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Padre Suárez (Ayunt. Oviedo)','43.358816','-5.842706','Tipo 2','Socket Only','22kw'),
+	 ('Calle Santa Susana (Ayunt. Oviedo)','43.359381','-5.850103','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Jerónimo Ibrán (Ayunt. Oviedo)','43.368216','-5.85088','Tipo 2','Tethered Connector','22kw'),
+	 ('Parking centro comercial Salesas','43.367123','-5.849158','Tipo 2','Socket Only','7.4kw'),
+	 ('Mercadona Oviedo','43.367899','-5.848768','Tipo 2','Socket Only','3.7kw'),
+	 ('Parking APK2 Longoria Carbajal','43.364628','-5.848457','Tipo 2','Socket Only','7.4kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Calle Foncalada','43.365029','-5.846421','Tipo 2','Socket Only','22kw'),
+	 ('Calle Foncalada (Ayunt. Oviedo)','43.36595','-5.846263','Tipo 2','Tethered Connector','22kw'),
+	 ('Parking Calle Víctor Chávarri','43.364754','-5.843685','Tipo 2','Socket Only','22kw'),
+	 ('Hotel Regente','43.36424','-5.8428','Tipo 2','Socket Only','22kw'),
+	 ('Calle Fernando Vela (Ayunt. Oviedo)','43.366614','-5.842421','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Melquíades Cabal (Ayunt. Oviedo)','43.36798','-5.842407','Tipo 2','Socket Only','22kw'),
+	 ('Avenida de Torrelavega (Ayunt. Oviedo)','43.36386','-5.836144','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Joaquín Blume (Ayunt. Oviedo)','43.366095','-5.831111','Tipo 2','Tethered Connector','22kw'),
+	 ('Parking Pumarín','43.369032','-5.847378','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle Palmira Villa González-Río (Ayunt. Oviedo)','43.370009','-5.847156','Tipo 2','Tethered Connector','22kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Univ. Oviedo Del Milán (Zunder)','43.368743','-5.841367','Tipo 2','Socket Only','7.4kw'),
+	 ('Avenida del Cantábrico (Ayunt. Oviedo)','43.372561','-5.843198','Tipo 2','Socket Only','22kw'),
+	 ('Lidl Oviedo','43.377407','-5.834581','Tipo 2','Socket Only','11kw'),
+	 ('Calle Juan López-Arranz (Ayunt. Oviedo)','43.375138','-5.831921','CSS','Socket Only','100kw'),
+	 ('Carretera del Rubín (Ayunt. Oviedo)','43.372927','-5.825502','Tipo 2','Socket Only','22kw'),
+	 ('Alimerka Prado de la Vega','43.379877','-5.816731','Tipo 2','Socket Only','22kw'),
+	 ('Calle Daniel Moyano (Ayunt. Oviedo)','43.382264 ','-5.824005','Tipo 2','Tethered Connector','22kw'),
+	 ('Calle Sabino Álvarez Gendín (Ayunt. Oviedo)','43.38322','-5.818248','Tipo 2','Socket Only','22kw'),
+	 ('Centro Asturiano de Oviedo','43.380473','-5.874379','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle Luis Suárez «Ximielga» (Ayunt. Oviedo)','43.377957','-5.795391','Tipo 2','Tethered Connector','22kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Gijón - Oriental','43.540492','-5.697004','Tipo 2','Socket Only','7.5kw'),
+	 ('Lidl Gijón','43.535017','-5.692192','Tipo 2','Socket Only','11kw'),
+	 ('E.S. Ballenoil La Calzada','43.535035','-5.690133','Tipo 2','Socket Only','22kw'),
+	 ('El Corte Inglés Hipercor (Gijón)','43.524065','-5.679222','CEE 7/4;Schuko;Tipo F','Socket Only','3.7kw'),
+	 ('Parque de Carlos Marx','43.536609','-5.6726','Tipo 2','Socket Only','22kw'),
+	 ('Mercadona Gijón','43.537896','-5.672012','Tipo 2','Socket Only','3.7kw'),
+	 ('Parking Espigón','43.54325','-5.666568','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle Álvarez Garaya 10','43.541324','-5.665551','CCS,Tipo 2,CHAdeMO','Socket Only,Tethered Connection','22kw,43kw,50kw'),
+	 ('Calle Álvaro de Albornoz','43.537916','-5.66642','Tipo 2','Socket Only','22kw'),
+	 ('Calle Juan Alvargonzalez','43.53031','-5.668825','Tipo 2','Socket Only','7.4kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Avenida Gaspar García Laviana','43.525782','-5.665873','Tipo 2','Socket Only','22kw'),
+	 ('Calle Río de Oro','43.527558','-5.663792','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle La Argandona','43.535503','-5.663979','Tipo 2','Socket Only','22kw'),
+	 ('APK2 Parking Plaza de Europa','43.538483','-5.663596','Tipo 2','Socket Only','22kw'),
+	 ('Calle Periodista Adeflor','43.538026','-5.661913','Tipo 2','Socket Only','7.3kw'),
+	 ('Parking Jardines del Naútico (Telepark)','43.542192','-5.660199','Tipo 2','Socket Only','22kw'),
+	 ('Antigua Pescaderia Municipal','43.544436','-5.662169','Tipo 2','Socket Only','22kw'),
+	 ('Plaza San Miguel','43.539997','-5.658071','Tipo 2','Socket Only','7.4kw'),
+	 ('Parking Zarracina','43.536984','-5.656813','Tipo 2','Socket Only','7.4kw'),
+	 ('8D Calle Peñalba','43.535414','-5.654622','Tipo 2','Socket Only','7.4kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Parking Plaza Begoña','43.537385','-5.660194','Tipo 2','Socket Only','7.4kw'),
+	 ('7 Avenida de El Llano','43.534402','-5.66031','Tipo 2','Socket Only','22kw'),
+	 ('Calle Canga Argüelles','43.538651','-5.650905','Tipo 2','Socket Only','7.4kw'),
+	 ('Parking Parque de la Fábrica de Gas','43.539631','-5.651187','Tipo 2','Socket Only','22kw'),
+	 ('Calle Manso','43.53921','-5.647931','Tipo 2','Tethered Connector','7.4kw'),
+	 ('Acceso al Parque','43.537964','-5.646362','Tipo 2','Socket Only','7.4kw'),
+	 ('Calle Avelino González Mallada','43.532691','-5.649169','Tipo 2','Socket Only','7.4kw'),
+	 ('Mercadona Gijón Pintor Luis Pardo','43.530786','-5.64654','Tipo 2','Socket Only','3.7kw'),
+	 ('El Molinón-Enrique Castro Quini','43.534971','-5.637534','CSS,CHAdeMO,Tipo 2','Socket Only,Tethered Connector','7.4kw,43kw,50kw'),
+	 ('Cámara de Comercio','43.541552','-5.636082','Tipo 2','Socket Only','22kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('E.S. Cepsa Gijón (Zunder)','43.524606','-5.64138','CSS,CHAdeMO,Tipo 2','Socket Only,Tethered Connector','7.4kw,43kw,50kw'),
+	 ('Univ. Oviedo EPI (EDP)','43.524216','-5.636648','Tipo 2,CSS,CHAdeMO','Socket Only,Tethered Connector','43kw,50kw,7.4kw'),
+	 ('Univ. Oviedo Ed. INTRA','43.525522','-5.62373','Tipo 2,CSS,CHAdeMO','Socket Only,Tethered Connector','22kw,50kw,7.4kw'),
+	 ('Univ. Oviedo Marina Civil Gijón (Zunder)','43.525599','-5.629147','CSS','Socket Only','20kw,100kw'),
+	 ('Univ. Oviedo Biblioteca Gijón (Zunder)','43.5248','-5.625256','CSS','Socket Only','25kw'),
+	 ('Univ. Oviedo Aulario Sur (Zunder)','43.523529','-5.623661','CSS','Socket Only','25kw'),
+	 ('Univ. Laboral Aparcamiento Parque Científico y Tecnológico','43.523476','-5.613044','Tipo 2','Socket Only','7.4kw'),
+	 ('Universidad Laboral','43.524288 ','-5.612864','Tipo 2,CSS,CHAdeMO','Socket Only,Tethered Connector','43kw,45kw,50kw,7.4kw'),
+	 ('Tanatorio Cabueñes','43.524776','-5.609279','Tipo 2,CSS','Socket Only','120kw,22kw,7.4kw'),
+	 ('Club De Golf Castiello','43.502059','-5.6387','Tipo 2','Socket Only','22kw');
+INSERT INTO Cargadores (Nombre,cordX,cordY,Cabezal,Tipo,Potencia) VALUES
+	 ('Colegio Corazón de María Gijón','43.516852','-5.663061','CSS,Tipo 2','Socket Only','7.4kw,60kw');
