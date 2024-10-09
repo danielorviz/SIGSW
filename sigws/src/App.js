328a93625b525fd3331f0c88afcf322ac3d95147
Katherine Gomez <kat.gomez.z@gmail.com>
2024-10-09 19:26:15 +0200

    added filter component

--- sigws/src/App.js
+++ sigws/src/App.js
@@ -4,18 +4,29 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import MapComponent from './components/MapComponent';
+import FilterComponent from "./components/filters";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Routes>
-        <Route exact path="/" element={<MapComponent geojson={null} />} />
-        <Route exact path="/oviedo" element={<MapComponent geojson={null} />} />
-          <Route exact path="/gijon" element={<MapComponent geojson={null} />} />
-        </Routes>
-        <Footer />
+        <Header/>
+        <div className="main-container">
+          {/* Left: Filter component */}
+          <div className="filter-container">
+            <FilterComponent/>
+          </div>
+
+          {/* Right: Map component */}
+          <div className="map-container">
+            <Routes>
+              <Route exact path="/" element={<MapComponent geojson={null}/>}/>
+              <Route exact path="/oviedo" element={<MapComponent geojson={null}/>}/>
+              <Route exact path="/gijon" element={<MapComponent geojson={null}/>}/>
+            </Routes>
+          </div>
+        </div>
+        <Footer/>
       </Router>
     </div>
   );
